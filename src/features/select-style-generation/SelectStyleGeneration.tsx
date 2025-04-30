5d9529f31510ed489d917b8ebfe1ecfadc95831e
Zong123558z <zong1235582@gmail.com>
2025-04-30 16:02:57 +0300

    fix

--- src/features/select-style-generation/SelectStyleGeneration.tsx
+++ src/features/select-style-generation/SelectStyleGeneration.tsx
@@ -5,13 +5,13 @@ import { CreatorMode } from '../creator-mode/CreatorMode'
 import { useEffect, useState } from 'react'
 import { useCreateGenerationsMutation } from '@/entities/generations/api/generations.api'
 import { useAppSelector } from '@/views/store'
-import { useLazyGetStylesQuery } from '@/entities/styles/api/styles.api'
+import { useGetStylesQuery } from '@/entities/styles/api/styles.api'
 
 
 export const SelectStyleGeneration = () => {
   const { resolution, creatorMode, activeProfileId, activeSubcategoryId } = useAppSelector(state => state.main.accountData)
-  const [getStyles, { data: style }] = useLazyGetStylesQuery()
-  const [activeStyleId, setActiveStyleId] = useState<number>(0)
+  const { data: style } = useGetStylesQuery()
+  const [activeStyleId, setActiveStyleId] = useState<number>(1)
   const [prompt, setPrompt] = useState('')
   const [isFocusInput, setIsFocusInput] = useState(false)
 
@@ -45,8 +45,10 @@ export const SelectStyleGeneration = () => {
   }
 
   useEffect(() => {
-    getStyles().then(data => data.data?.categories[0].id || 0)
-  }, [])
+    if(style?.categories?.[0].id) {
+      setActiveStyleId(style.categories[0].id)
+    }
+  }, [style])
 
   return (
     <div className="flex flex-col gap-[2.43vw] mb-[15.78vw] items-end w-full bg-transparent relative z-[2]">
