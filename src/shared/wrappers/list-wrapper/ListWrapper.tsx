b12ef33502e949a1afc08963546c85ef897c351a
Zong123558z <zong1235582@gmail.com>
2025-05-01 12:58:06 +0300

    12:58

--- src/shared/wrappers/list-wrapper/ListWrapper.tsx
+++ src/shared/wrappers/list-wrapper/ListWrapper.tsx
@@ -4,27 +4,11 @@ import { DependencyList, PropsWithChildren, useEffect, useRef, useState } from "
 
 interface IProps {
   className?: string
-  depsForScroll?: DependencyList
 }
 
-export const ListWrapper = ({ children, className, depsForScroll }: PropsWithChildren<IProps>) => {
+export const ListWrapper = ({ children, className }: PropsWithChildren<IProps>) => {
   const containerRef = useRef<HTMLDivElement | null>(null)
   const { height } = useGetHeight({ containerRef })
-
-  // scroll to bottom
-  if(depsForScroll) {
-    useEffect(() => {
-      const timeout = setTimeout(() => {
-        if (containerRef.current) {
-          containerRef.current.scrollTop = containerRef.current.scrollHeight;
-        }
-      }, 500)
-  
-      return () => {
-        clearTimeout(timeout)
-      }
-    }, [depsForScroll]);
-  }
 
   return (
     <div style={{ height, paddingBottom: height ? '15px': '0px' }} ref={containerRef} className={`overflow-y-auto ${className ? className: ''}`}>
