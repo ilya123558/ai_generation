0800522557de40c97dc56e638768025bd4074a81
Zong123558z <zong1235582@gmail.com>
2025-05-01 13:53:04 +0300

    13:53

--- src/features/chat-list/ChatList.tsx
+++ src/features/chat-list/ChatList.tsx
@@ -3,26 +3,19 @@ import { useGetGenerationsChatQuery } from '@/entities/generations/api/generatio
 import { ChatItemUser } from '@/shared/chat-item-user/ChatItemUser'
 import { ChatItem } from '@/shared/chat-item/ChatItem'
 import { ListWrapper } from '@/shared/wrappers/list-wrapper/ListWrapper'
-import { useEffect, useRef } from 'react'
 
 export const ChatList = () => {
   const { data } = useGetGenerationsChatQuery({limit: 50})
 
-  useEffect(() => {
-    if(data) {
-      alert(JSON.stringify(data))
-    }
-  }, [data])
-
   return (
     <ListWrapper className='mb-[10px]' >
       <ul className='flex flex-col gap-[5.88vw]'>
-        {data?.generations && data.generations.reverse().map((chatItem, index) => {
-          if(chatItem.sender === 'user') return (
-            <ChatItemUser key={index} {...chatItem} />
-          )
+        {data?.generations?.slice().reverse().map((chatItem) => {
+          if (chatItem.sender === 'user') {
+            return <ChatItemUser key={chatItem.id} {...chatItem} />
+          }
 
-          return <ChatItem key={index} {...chatItem} />
+          return <ChatItem key={chatItem.id} {...chatItem} />
         })}
       </ul>
     </ListWrapper>
