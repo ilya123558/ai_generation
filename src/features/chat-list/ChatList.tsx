b99b44b686ace3b997798e9505c13265e92dfc86
Zong123558z <zong1235582@gmail.com>
2025-05-01 13:40:56 +0300

    13:40

--- src/features/chat-list/ChatList.tsx
+++ src/features/chat-list/ChatList.tsx
@@ -1,23 +1,23 @@
 'use client'
-import { useLazyGetGenerationsChatQuery } from '@/entities/generations/api/generations.api'
+import { useGetGenerationsChatQuery } from '@/entities/generations/api/generations.api'
 import { ChatItemUser } from '@/shared/chat-item-user/ChatItemUser'
 import { ChatItem } from '@/shared/chat-item/ChatItem'
 import { ListWrapper } from '@/shared/wrappers/list-wrapper/ListWrapper'
 import { useEffect, useRef } from 'react'
 
 export const ChatList = () => {
-  const [getGenerationsChat, { data }] = useLazyGetGenerationsChatQuery()
+  const { data } = useGetGenerationsChatQuery({limit: 50})
 
-  useEffect(() => {
-    (async() => {
-      await getGenerationsChat({limit: 50})
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async() => {
+  //     await getGenerationsChat({limit: 50})
+  //   })()
+  // }, [])
 
   if(!(data && data.generations)) return <></>
 
   return (
-    <div className='mb-[10px]' >
+    <ListWrapper className='mb-[10px]' >
       <ul className='flex flex-col gap-[5.88vw]'>
         {data.generations.reverse().map((chatItem, index) => {
           if(chatItem.sender === 'user') return (
@@ -27,6 +27,6 @@ export const ChatList = () => {
           return <ChatItem key={index} {...chatItem} />
         })}
       </ul>
-    </div>
+    </ListWrapper>
   )
 }
