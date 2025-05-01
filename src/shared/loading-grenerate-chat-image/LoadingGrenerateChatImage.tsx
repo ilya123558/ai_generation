c7afa764c581c3c010d7ce3b3a2b7bd71590258d
Zong123558z <zong1235582@gmail.com>
2025-05-01 15:42:18 +0300

    15:42

--- src/shared/loading-grenerate-chat-image/LoadingGrenerateChatImage.tsx
+++ src/shared/loading-grenerate-chat-image/LoadingGrenerateChatImage.tsx
@@ -5,7 +5,7 @@ import { useAppSelector } from "@/views/store";
 export const LoadingGrenerateChatImage = () => {
   const { displayPrompt } = useAppSelector(state => state.main.meta)
 
-  if(!displayPrompt) return
+  if(displayPrompt === null) return <></>
 
   return (
     <div className="w-full h-[100px] bg-red">
