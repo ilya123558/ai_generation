ee2b21c4e590175f3ead34db0a4ac0cf6e42b291
Zong123558z <zong1235582@gmail.com>
2025-05-01 14:30:04 +0300

    14:30

--- src/utils/libs/formatChatText.ts
+++ src/utils/libs/formatChatText.ts
@@ -2,7 +2,7 @@ export const formatChatText = (text: string) => {
   const textArray = text.split('<b>Стиль:</b>')[1].split('\n')
 
   return {
-    style: textArray[1] ? textArray[1].trim() : '',
+    style: textArray[0] ? textArray[0].trim() : '',
     text: textArray[1] ? textArray[1].trim() : '',
   }
 }
