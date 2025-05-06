b89751fdb55044aed655a1e5db38cba7bc623013
Zong123558z <zong1235582@gmail.com>
2025-05-06 09:37:07 +0300

    20:07

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -2,7 +2,7 @@ import { OnboardingSlider } from "@/widgets/onboarding-slider/OnboardingSlider";
 
 export default function Page() {
   return (
-    <section className="bg-[#23262F] overflow-hidden relative">
+    <section className="bg-[#23262F] fixed overflow-hidden w-screen h-screen top-0 left-0">
       <OnboardingSlider />
       <div className="absolute bottom-[-180vw] w-[200vw] left-[-51vw] aspect-square bg-[#1E7FFF] rounded-full blur-[100px]"></div>
     </section>
