f29410ee38620bd9558ae15770a62509ad231401
Zong123558z <zong1235582@gmail.com>
2025-05-07 16:56:22 +0300

    fix

--- src/shared/wrappers/provider-wrapper/ProviderWrapper.tsx
+++ src/shared/wrappers/provider-wrapper/ProviderWrapper.tsx
@@ -1,10 +1,29 @@
 'use client'
 import { store } from '@/views/store'
-import { viewportSafeAreaInsets } from '@telegram-apps/sdk'
 import { PropsWithChildren, useEffect } from 'react'
 import { Provider } from 'react-redux'
 
 export const ProviderWrapper = ({ children }: PropsWithChildren) => {
+  useEffect(() => {
+    const preventPinchZoom = (e: TouchEvent) => {
+      if (e.touches.length > 1) {
+        e.preventDefault()
+      }
+    }
+
+    const preventGesture = (e: Event) => {
+      e.preventDefault()
+    }
+
+    document.addEventListener("touchmove", preventPinchZoom, { passive: false })
+    document.addEventListener("gesturestart", preventGesture, { passive: false })
+
+    return () => {
+      document.removeEventListener("touchmove", preventPinchZoom)
+      document.removeEventListener("gesturestart", preventGesture)
+    }
+  }, [])
+
   useEffect(() => {
     const metaViewport = document.createElement('meta');
     metaViewport.name = 'viewport';
@@ -28,26 +47,23 @@ export const ProviderWrapper = ({ children }: PropsWithChildren) => {
 
   useEffect(() => {
     const checkTelegramWebApp = setInterval(() => {
-      try{
-        if (window.Telegram && window.Telegram.WebApp) {
-          const tg = window.Telegram.WebApp;
-  
-          const isAndroid = typeof navigator !== 'undefined' && navigator.userAgent ? /Android/i.test(navigator.userAgent) : false;
-          const isIos = typeof navigator !== 'undefined' && navigator.userAgent ? /iPhone|iPad|iPod/i.test(navigator.userAgent) : false;
-          const isDesktop = isAndroid || isIos ? false : true
-  
-          if(!isDesktop) {
-            tg.requestFullscreen();
-            tg.disableSwipes()
-          }
-  
-          const topSafeArea = isAndroid ? 70 : 80;
-          document.body.style.marginTop = `${isDesktop ? 0 : topSafeArea}px`;
-  
-          clearInterval(checkTelegramWebApp);
+      if (window.Telegram && window.Telegram.WebApp) {
+        const tg = window.Telegram.WebApp;
+
+        const isAndroid = typeof navigator !== 'undefined' && navigator.userAgent ? /Android/i.test(navigator.userAgent) : false;
+        const isIos = typeof navigator !== 'undefined' && navigator.userAgent ? /iPhone|iPad|iPod/i.test(navigator.userAgent) : false;
+        const isDesktop = isAndroid || isIos ? false : true
+
+        if(!isDesktop) {
+          tg.requestFullscreen();
+          tg.disableSwipes?.()
         }
+
+        const topSafeArea = isAndroid ? 70 : 80;
+        document.body.style.marginTop = `${isDesktop ? 0 : topSafeArea}px`;
+
+        clearInterval(checkTelegramWebApp);
       }
-      catch (e) {}
     }, 100);
   
     return () => clearInterval(checkTelegramWebApp);
