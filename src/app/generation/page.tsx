e25e0da153466e3d39f7b625a89f8aa1cb35cc43
Zong123558z <zong1235582@gmail.com>
2025-05-09 15:04:55 +0300

    fix

--- src/app/generation/page.tsx
+++ src/app/generation/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 import { HeaderWithIcons } from '@/features/header-with-icons/HeaderWithIcons'
+import { useFixViewportHeight } from '@/utils/hooks/useFixViewportHeight'
 import { GenerationChat } from '@/widgets/generation-chat/GenerationChat'
 
 export default function Page() {
+  useFixViewportHeight()
+
   return (
     <section className="relative h-screen w-screen overflow-hidden">
       <div className="absolute w-full h-full flex items-center justify-center z-[-1] fs-40 font-semibold opacity-5 select-none pointer-events-none">
