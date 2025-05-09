ac4ba140b16586f36bf84fd0e848301cd2b7fa07
Zong123558z <zong1235582@gmail.com>
2025-05-09 15:12:47 +0300

    fix

--- src/utils/hooks/useFixViewportHeight.tsx
+++ src/utils/hooks/useFixViewportHeight.tsx
@@ -2,27 +2,27 @@ import { useEffect } from "react";
 import { useTelegram } from "./useTelegram";
 
 export const useFixViewportHeight = () => {
-  const { webApp } = useTelegram()
+  const { webApp } = useTelegram();
 
   useEffect(() => {
-    if (!webApp) return
-    
+    if (!webApp) return;
+
     const updateStableViewportHeight = () => {
       const stableHeight = webApp.viewportStableHeight;
       document.documentElement.style.setProperty('--tg-viewport-stable-height', `${stableHeight}px`);
     };
-    webApp.onEvent('viewportChanged', (event) => {
-      if (event.isStateStable) {
-        setTimeout(() => {
-          updateStableViewportHeight();
-        }, 200)
-      }
-    });
 
+    const handleViewportChange = (event: any) => {
+      if (event.isStateStable) {
+        updateStableViewportHeight();
+      }
+    };
+
+    webApp.onEvent('viewportChanged', handleViewportChange);
     updateStableViewportHeight();
 
     return () => {
-      webApp.offEvent('viewportChanged', updateStableViewportHeight);
+      webApp.offEvent('viewportChanged', handleViewportChange);
     };
   }, [webApp]);
 };
