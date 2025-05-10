4f2d253f451cb74339e56ffded851d82a123067f
Zong123558z <zong1235582@gmail.com>
2025-05-10 08:24:39 +0300

    fix

--- src/app/generation/page.tsx
+++ src/app/generation/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 import { HeaderWithIcons } from '@/features/header-with-icons/HeaderWithIcons'
 import { useFixViewportHeight } from '@/utils/hooks/useFixViewportHeight'
+import { useGetDevice } from '@/utils/hooks/useGetDevice'
 import { GenerationChat } from '@/widgets/generation-chat/GenerationChat'
 
 export default function Page() {
   useFixViewportHeight()
+  const { getTelegramTopPaddingValue } = useGetDevice()
 
   return (
-    <section className="fixed left-0 right-0 h-screen w-screen overflow-hidden bg-red">
+    <section style={{paddingTop: getTelegramTopPaddingValue()}} className="fixed left-0 top-0 h-screen w-screen overflow-hidden">
       <div className="absolute w-full h-full flex items-center justify-center z-[-1] fs-40 font-semibold opacity-5 select-none pointer-events-none">
         Photiqe
       </div>
