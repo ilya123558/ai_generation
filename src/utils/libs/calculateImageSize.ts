62e7c55ae247106b2bb4d22959fc24882e8a03b2
Zong123558z <zong1235582@gmail.com>
2025-05-27 14:35:10 +0300

    fix

--- src/utils/libs/calculateImageSize.ts
+++ src/utils/libs/calculateImageSize.ts
@@ -4,7 +4,7 @@ export const calculateImageSize = (resolution: TResolution, maxWidth = 203) => {
   const aspectRatios: { [key in typeof resolution]: number[] } = {
     "4:3": [4, 3],
     "16:9": [16, 9],
-    "2:35:1": [2.35, 1],
+    "2.35:1": [2.35, 1],
     "9:16": [9, 16],
     "2:3": [2, 3],
     "1:1": [1, 1],
