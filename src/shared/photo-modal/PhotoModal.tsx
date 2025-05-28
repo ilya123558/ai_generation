dd109e4d43cc556a3403a5e58b4cf5aa751d1f76
ilya123558 <ilya123558ilya@gmail.com>
2025-05-28 11:12:44 +0300

    fix

--- src/shared/photo-modal/PhotoModal.tsx
+++ src/shared/photo-modal/PhotoModal.tsx
@@ -17,32 +17,15 @@ export const PhotoModal = ({isOpen, setIsOpen, handleDelete, photo}: IProps) => 
   const [isDelete, setIsDelete] = useState(false)
   const { webApp } = useTelegram()
 
-  // const handleDownload = async() => {
-  //   if(!webApp) return;
+  const handleDownload = async() => {
+    if(!webApp) return;
 
-  //   webApp.downloadFile({
-  //     file_name: 'ai_image.jpg',
-  //     url: photo
-  //   })
-  // }
+    webApp.downloadFile({
+      file_name: 'ai_image.jpg',
+      url: photo
+    })
+  }
 
-  const handleDownload = async () => {
-    const url = photo; // Прямой URL фотографии
-
-    // Создаем элемент <a>, который будет использоваться для скачивания
-    const link = document.createElement('a');
-    link.href = url;          // Устанавливаем ссылку на изображение
-    link.download = 'ai_image.jpg'; // Имя файла при скачивании
-
-    // Имитируем клик по ссылке, чтобы скачать файл
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-    // Возвращаем ссылку на изображение для дальнейшего использования
-    return url;
-  };
- 
   const handleRepost = async () => {
     const url = encodeURIComponent(photo);
     const text = encodeURIComponent('Фотография была сгенерирована с помощью @new_ai444_bot');
